532344322e0f370d9660f85679b997218489b7f6
adityapk <adityapk@gmail.com>
2021-12-09 10:48:04 -0800

    Remove debug

--- contracts/ImpishDao.sol
+++ contracts/ImpishDao.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
