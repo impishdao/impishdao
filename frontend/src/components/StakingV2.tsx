1afd3caca4af9f9da8a2e05620cf4fa35ea48ef0
adityapk <adityapk@gmail.com>
2022-01-28 11:06:51 -0800

    Update FAQ

--- frontend/src/components/StakingV2.tsx
+++ frontend/src/components/StakingV2.tsx
@@ -882,8 +882,17 @@ export function SpiralStaking(props: SpiralStakingProps) {
           <div className="mb-3">
             <span style={{ fontWeight: "bold", color: "#ffd454" }}>How does the Staking work?</span>
             <br />
-            "Chapter 2: The SpiralBits" lets you stake your Impish Spiral NFTs and your RandomWalkNFTs to earn
-            SPIRALBITS tokens. Staking is the process of depositing your Impish Spiral or RandomWalkNFT into a staking
+            "Chapter 2: The SpiralBits" lets you stake your Impish NFTs and tokens to earn
+            SPIRALBITS tokens. The currently supported NFTs and Tokens are:
+            <ul>
+              <li>RandomWalk NFT</li>
+              <li>Impish Spiral</li>
+              <li>Impish Crystals</li>
+              <li>IMPISH tokens</li>
+              <li>SPIRALBITS tokens</li>
+            </ul>
+            
+            Staking is the process of depositing your NFTs and tokens into a staking
             contract, and earning a stream of SPIRALBITS tokens for the duration that the NFTs are kept in the staking
             contracts.
             <br />
@@ -892,40 +901,25 @@ export function SpiralStaking(props: SpiralStakingProps) {
             Impish Chapters.
             <br />
             Staking a Impish Spiral earns you a 10 SPIRALBITS per minute or 14.4K SPIRALBITS per day. Staking a
-            RandomWalkNFT earns you 1 SPIRALBITS per minute, or 1.4K SPIRALBITS per day.
+            RandomWalkNFT earns you 1 SPIRALBITS per minute, or 1.4K SPIRALBITS per day. Staking a fully grown crystal earns you
+            7,2K SPIRALBITS per day.
             <br />
             <br />
             You can stake, unstake and/or withdraw your accumulated SPIRALBITS at any time, there are no lockins.
-            However, you might need to carefully plan your staking and withdrawal strategy to take advantage of the
-            SPIRALBITS bonuses.
+            However, note that if you have any Crystals that are growing, you might want to harvest them first, allowing them 
+            to grow to full size.
           </div>
 
           <div className="mb-3">
-            <span style={{ fontWeight: "bold", color: "#ffd454" }}>What are the SPIRALBITS bonuses?</span>
+            <span style={{ fontWeight: "bold", color: "#ffd454" }}>How do I grow my Crystals?</span>
             <br />
-            At the moment you withdraw your accumulated SPIRALBITS, you are awarded a bonus, which is calculated as
-            follows:
-            <ul>
-              <li>
-                For staked Spirals - The percentage of RandomWalkNFTs staked at the time of the withdrawal determines
-                your bonus percentage{" "}
-              </li>
-              <li>
-                For staked RandomWalkNFTs - The percentage of Spirals staked at the time of the withdrawal determines
-                your bonus percentage{" "}
-              </li>
-            </ul>
-            <br />
-            For example, if you have staked 1 Spiral, and have accumulated 100k SPIRALBITS so far, and you decide to
-            withdraw your earned SPIRALBITS into your wallet. At the instant you withdraw, the staking contract
-            determines what percentage of RandomWalkNFTs have been staked in Staking contract. Lets say 1000
-            RandomWalkNFTs out of a total of 3608 RandomWalkNFTs are staked, your bonus percentage will be 1000 / 3608 =
-            27.7%, so you will be awarded 27.7% extra SPIRALBITS, for a total of 100k + 27.7k = 127.7k SPIRALBITS.
-            <br />
-            <br />
-            Similarly, if you have staked RandomWalkNFTs and have accumulated SPIRALBITS, at the instant you withdraw,
-            the contract calculates the number of Spirals staked, divided by the total number of Spirals in existance,
-            calculates the percentage, and awards that percentage of SPIRALBITS as the bonus.
+            If you stake a Impish Crystal that is not fully grown yet (i.e., not size 100), then it will automatically absorb 
+            SPIRALBITS rewards earned by the other NFTs and tokens you have staked and grow. 
+            <br/>
+            After the Crystal has grown to full size, you may "Harvest" it, turning it into a fully grown Crystal. Fully grown
+            Crystals will then start earning Staking rewards themselves
+            <br/><br/>
+            Of course, you can unstake any NFT or Token at any time - There are no lockins.
           </div>
 
           <div className="mb-3">
@@ -960,27 +954,7 @@ export function SpiralStaking(props: SpiralStakingProps) {
             </ul>
           </div>
 
-          <div className="mb-3">
-            <span style={{ fontWeight: "bold", color: "#ffd454" }}>
-              When should I withdraw my accumulated SPIRALBITS
-            </span>
-            <br />
-            Technically, you can withdraw your staked NFTs or accumulated SPIRALBITS at any time - there are no lockups.
-            <br />
-            <br />
-            However, if you have staked Spirals, you might want to get as many other people staking RandomWalkNFTs as
-            possible, which increases your bonus %.
-            <br />
-            <br />
-            Similarly, if you have staked RandomWalkNFTs, you should encourage as many Spiral owners as you know to
-            stake their Spirals, so you can get as large a bonus % as possible.
-            <br />
-            <br />
-            Of course, if a very large % of Spirals and RandomWalkNFTs are staked, they are also earning SPIRALBITS,
-            which might "dilute" your SPIRALBITS, so you need to decide what the best strategy for you is!
-          </div>
-
-          <div className="mb-3">
+                   <div className="mb-3">
             <span style={{ fontWeight: "bold", color: "#ffd454" }}>What utility do SPIRALBITS have?</span>
             <br />
             SPIRALBITS are the utility token for the rest of the Impish Chapters. All details will be released 2nd week
@@ -1008,7 +982,7 @@ export function SpiralStaking(props: SpiralStakingProps) {
             <br />
             The Staking contracts for &nbsp;
             <a
-              href="https://arbiscan.io/address/0xfa798e448db7987a5d7ab3620d7c3d5ecb18275e#code"
+              href="https://arbiscan.io/address/0x2069cB988d5B17Bab70d73076d6F1a9757A4f963#code"
               rel="noreferrer"
               target="_blank"
               style={{ color: "white" }}
